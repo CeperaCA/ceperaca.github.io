ab39cf6e445d830b16bb2ede96ab98526426ed32
Сергей <sergej-silchenko@yandex.ru>
2024-12-23 15:53:49 +0300

    Delete OrientationLandscape.js

--- lampa/OrientationLandscape.js
+++
@@ -1,8 +0,0 @@
-(function () {
-	'use strict';
-	
-	if (document.getElementsByTagName("video")[0]) {
-		document.body.classList.remove('orientation--portrait');
-		document.body.classList.add('orientation--landscape');
-	}
-})();
