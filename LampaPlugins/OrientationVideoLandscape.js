3d4a53f582cb97ca3897a5035810dc021e451665
Сергей <sergej-silchenko@yandex.ru>
2024-12-24 18:23:09 +0300

    Create OrientationVideoLandscape.js

---
+++ LampaPlugins/OrientationVideoLandscape.js
@@ -0,0 +1,21 @@
+(function () {
+	'use strict';
+	
+	// Используем MutationObserver для отслеживания добавления видео на страницу
+	const observer = new MutationObserver(function(mutationsList, observer) {
+	mutationsList.forEach(function(mutation) {
+		// Для каждого добавленного элемента проверяем, является ли он видео
+		mutation.addedNodes.forEach(function(node) {
+			if (node.tagName === 'VIDEO') {
+				node.style.cssText = "width: 100vh; height: 100vw; transform: rotate(90deg); transform-origin: bottom left; margin-top: -100vw;";
+			}
+		});
+	});
+	});
+
+	// Настройки для наблюдения за DOM
+	observer.observe(document.body, {
+		childList: true,      // Следим за добавлением новых элементов
+		subtree: true         // Следим за вложенными изменениями
+	});
+})();
