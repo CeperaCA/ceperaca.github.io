a2a1a2e782137a129f28ebf07079d9e7308f6947
Сергей <sergej-silchenko@yandex.ru>
2024-12-24 18:23:00 +0300

    Delete OrientationVideoLandscape.js

--- lampaplugins/OrientationVideoLandscape.js
+++
@@ -1,6 +0,0 @@
-(function () {
-	'use strict';
-	
-	const sheet = document.styleSheets[0];
-	sheet.insertRule('video { width: 100vh; height: 100vw; transform: rotate(90deg); transform-origin: bottom left; margin-top: -100vw; }', sheet.cssRules.length);
-})();
