77360108e392bdd3e9200e141f85da1251ebd329
Сергей <sergej-silchenko@yandex.ru>
2024-12-25 10:25:27 +0300

    Update OrientationVideoLandscape.js

--- LampaPlugins/OrientationVideoLandscape.js
+++ LampaPlugins/OrientationVideoLandscape.js
@@ -14,7 +14,7 @@
 				if (video) {
 					// Применяем стили для горизонтальной ориентации видео
 					video.removeAttribute("style");
-					video.style.cssText = "width: 100vh; height: 100vw; transform: rotate(90deg); transform-origin: bottom left; margin-top: -100vw;";
+					//video.style.cssText = "width: 100vh; height: 100vw; transform: rotate(90deg); transform-origin: bottom left; margin-top: -100vw;";
 				}
 			}
 		});
