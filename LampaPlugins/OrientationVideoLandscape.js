2b7970144c0d29f27a6afc05a215b4acfd6d2229
Сергей <sergej-silchenko@yandex.ru>
2024-12-25 17:20:42 +0300

    Update OrientationVideoLandscape.js

--- LampaPlugins/OrientationVideoLandscape.js
+++ LampaPlugins/OrientationVideoLandscape.js
@@ -1,30 +1,33 @@
 (function () {
 	'use strict';
 
-	// Используем MutationObserver для отслеживания добавления видео на страницу
+	// Используем MutationObserver для отслеживания добавления элемента video на страницу
 	const observer = new MutationObserver(function(mutationsList, observer) {
 		mutationsList.forEach(function(mutation) {
-			// Для каждого добавленного элемента проверяем, является ли он видео
+			// Для каждого добавленного элемента проверяем, является ли он video
 			mutation.addedNodes.forEach(function(node) {
-				// Проверяем, если добавленный элемент имеет класс 'player'
+				// Проверяем, если добавленный элемент имеет класс player
 				if (node.classList && node.classList.contains('player')) {
-					// Получаем видео элемент
-					let video = node.querySelector('video');
+					// Проверяем, что это мобильное устройство в портретной ориентации
+					if (document.body.classList.contains('true--mobile orientation--portrait')) {
+						// Получаем video элемент
+						const video = node.querySelector('video');
 
-					if (video) {
-						setTimeout(function() {
-							// Применяем стили для горизонтальной ориентации видео
-							video.style.width           = "100vh";
-							video.style.height          = "100vw";
-							video.style.transform       = "rotate(90deg)";
-							video.style.transformOrigin = "bottom left";
-							video.style.marginTop       = "-100vw";
-							video.style.objectFit       = "cover"; // Убедимся, что видео не деформируется
-							
-							// Останавливаем наблюдение после применения стилей
-							observer.disconnect();
-						}, 1000);
-					}
+						if (video) {
+							setTimeout(function() {
+								// Применяем стили для горизонтальной ориентации элемента video
+								video.style.width           = "100vh";
+								video.style.height          = "100vw";
+								video.style.transform       = "rotate(90deg)";
+								video.style.transformOrigin = "bottom left";
+								video.style.marginTop       = "-100vw";
+								video.style.objectFit       = "cover"; // Убедимся, что элемент video не деформируется
+								
+								// Останавливаем наблюдение после применения стилей
+								observer.disconnect();
+							}, 1000);
+						}
+						}
 				}
 			});
 		});
