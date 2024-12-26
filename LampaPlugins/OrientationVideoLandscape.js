b3eb7c6fd71e1f054a9a848a3460a700a635cf9b
Сергей <sergej-silchenko@yandex.ru>
2024-12-26 18:01:44 +0300

    Update OrientationVideoLandscape.js

--- LampaPlugins/OrientationVideoLandscape.js
+++ LampaPlugins/OrientationVideoLandscape.js
@@ -2,7 +2,7 @@
 	'use strict';
 	
 	console.log('[OrientationVideoLandscape]: ', 'v0.1');
-	
+
 	// Используем MutationObserver для отслеживания добавления элементов на страницу
 	const observer = new MutationObserver(function(mutationsList, observer) {
 		mutationsList.forEach(function(mutation) {
@@ -10,13 +10,29 @@
 			mutation.addedNodes.forEach(function(node) {
 				// Проверяем, если добавленный элемент имеет класс player
 				if (node.classList && node.classList.contains('player')) {
-					console.log('[OrientationVideoLandscape]: ', node);
-					const panel = node.querySelector('.player-panel__right');
+					let panel = node.querySelector('.player-panel__right');
 
 					if (panel) {
-						console.log('[OrientationVideoLandscape]: ', panel.lastElementChild);
-						panel.append(panel.lastElementChild);
-						
+						let newDiv = document.createElement('div');
+						newDiv.className = 'player-panel__rotate button';
+						newDiv.innerHTML =
+`<svg width="25" height="23" viewBox="0 0 25 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<!-- Левая стрелка -->
+	<path d="M1,11 L3,8 L3,14 Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
+
+	<!-- Прямоугольник -->
+	<rect x="7" y="5" width="12" height="13" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
+
+	<!-- Правая стрелка -->
+	<path d="M24,11 L22,8 L22,14 Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
+</svg>`;
+
+						newDiv.addEventListener('click', function() {
+							console.log('[OrientationVideoLandscape]: ', 'Клик на новый элемент!');
+						});
+
+						panel.append(newDiv);
+
 						// Останавливаем наблюдение
 						observer.disconnect();
 					}
