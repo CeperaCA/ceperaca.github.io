0442eda978db04c7f16604768e38eb56f4037693
Сергей <sergej-silchenko@yandex.ru>
2024-12-26 16:40:03 +0300

    Update OrientationVideoLandscape.js

--- LampaPlugins/OrientationVideoLandscape.js
+++ LampaPlugins/OrientationVideoLandscape.js
@@ -1,33 +1,22 @@
 (function () {
 	'use strict';
 	
-	console.log('v0.1');
+	console.log('OrientationVideoLandscape v0.1');
 	
-	// Используем MutationObserver для отслеживания добавления элемента video на страницу
+	// Используем MutationObserver для отслеживания добавления элементов на страницу
 	const observer = new MutationObserver(function(mutationsList, observer) {
 		mutationsList.forEach(function(mutation) {
-			// Для каждого добавленного элемента проверяем, является ли он video
+			// Проверяем каждый добавленный элемент
 			mutation.addedNodes.forEach(function(node) {
 				// Проверяем, если добавленный элемент имеет класс player
 				if (node.classList && node.classList.contains('player')) {
-					// Получаем video элемент
-					const video = node.querySelector('video');
+					const panel = node.querySelector('.player-panel__right');
 
-					if (video) {
-						setTimeout(function() {
-							// Применяем стили для горизонтальной ориентации элемента video
-							//video.style.width           = '100vh';
-							//video.style.height          = '100vw';
-							video.style.transform       = 'rotate(90deg)';
-							//video.style.transformOrigin = 'bottom left';
-							//video.style.marginTop       = '-100vw';
-							//video.style.objectFit       = 'cover'; // Убедимся, что элемент video не деформируется
-							
-							console.log(video.style.transform);
-							
-							// Останавливаем наблюдение после применения стилей
-							observer.disconnect();
-						}, 1000);
+					if (panel) {
+						panel.append(panel.lastElementChild);
+						
+						// Останавливаем наблюдение
+						observer.disconnect();
 					}
 				}
 			});
