cbedc61788dfe7fbbe2fbb071cc665206d30299a
Сергей <sergej-silchenko@yandex.ru>
2024-12-27 14:31:56 +0300

    Update OrientationVideoLandscape.js

--- LampaPlugins/OrientationVideoLandscape.js
+++ LampaPlugins/OrientationVideoLandscape.js
@@ -1,10 +1,12 @@
 (function () {
 	'use strict';
-	
+
 	console.log('[OrientationVideoLandscape]: ', 'v0.1');
 
 	// Используем MutationObserver для отслеживания добавления элементов на страницу
 	const observer = new MutationObserver(function(mutationsList, observer) {
+        console.log('[OrientationVideoLandscape]: ', 'Старт отслеживания добавления элементов на страницу');
+
 		mutationsList.forEach(function(mutation) {
 			// Проверяем каждый добавленный элемент
 			mutation.addedNodes.forEach(function(node) {
@@ -16,25 +18,38 @@
 						let newDiv = document.createElement('div');
 						newDiv.className = 'player-panel__rotate button';
 						newDiv.innerHTML =
-`<svg width="25" height="23" viewBox="0 0 25 23" fill="none" xmlns="http://www.w3.org/2000/svg">
-	<!-- Левая стрелка -->
-	<path d="M1,11 L3,8 L3,14 Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
+							`<svg width="25" height="23" viewBox="0 0 50 50" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+								<path d="M32.97 5.03c6.53 3.1 11.22 9.45 11.93 16.97h3c-1.02-12.32-11.32-22-23.9-22-.45 0-.88.04-1.33.07l7.63 7.63 2.67-2.67zm-12.51-1.54c-1.17-1.17-3.07-1.17-4.24 0l-12.73 12.73c-1.17 1.17-1.17 3.07 0 4.24l24.04 24.04c1.17 1.17 3.07 1.17 4.24 0l12.73-12.73c1.17-1.17 1.17-3.07 0-4.24l-24.04-24.04zm9.2 38.89l-24.05-24.04 12.73-12.73 24.04 24.04-12.72 12.73zm-14.63.59c-6.53-3.1-11.22-9.45-11.93-16.97h-3c1.02 12.32 11.32 22 23.9 22 .45 0 .88-.04 1.33-.07l-7.63-7.63-2.67 2.67z"/>
+							</svg>`;
 
-	<!-- Прямоугольник -->
-	<rect x="7" y="5" width="12" height="13" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
-
-	<!-- Правая стрелка -->
-	<path d="M24,11 L22,8 L22,14 Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
-</svg>`;
-
+						// Добавляем обработчик события на клик
 						newDiv.addEventListener('click', function() {
-							console.log('[OrientationVideoLandscape]: ', 'Клик на новый элемент!');
+							console.log('[OrientationVideoLandscape]: ', 'Клик на кнопку поворота экрана');
+
+                            // Получаем видео элемент
+                            const video = document.body.querySelector('video');
+
+                            if (video) {
+                                // Применяем стили для горизонтальной ориентации видео
+                                if (!video.getAttribute('style') || video.style.transform == 'unset') {
+                                    video.style.width           = '100vh';
+                                    video.style.height          = '100vw';
+                                    video.style.transform       = 'rotate(90deg)';
+                                    video.style.transformOrigin = 'bottom left';
+                                    video.style.marginTop       = '-100vw';
+                                    video.style.objectFit       = 'cover'; // Убедимся, что видео не деформируется
+                                } else {
+                                    video.removeAttribute('style');
+                                }
+                            }
 						});
 
 						panel.append(newDiv);
+                        console.log('[OrientationVideoLandscape]: ', 'Добавлена кнопка поворота экранов');
 
 						// Останавливаем наблюдение
 						observer.disconnect();
+                        console.log('[OrientationVideoLandscape]: ', 'Остановка отслеживания добавления элементов на страницу');
 					}
 				}
 			});
